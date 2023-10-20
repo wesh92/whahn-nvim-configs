8e13700aa8f28072c7246da1765c4740de74bc9a
Wes Hahn <52631646+wesh92@users.noreply.github.com>
2023-10-20 08:35:01 -0400

    nvim done for now!

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,86 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.setup()
+
+lsp.ensure_installed({
+	'pylsp',
+	'lua_ls',
+})
+
+
+
+local lspconfig = require('lspconfig')
+
+lspconfig.lua_ls.setup({
+  settings = {
+    Lua = {
+      completion = {
+        callSnippet = "Replace"
+      }
+    }
+  }
+})
+
+lspconfig.pylsp.setup {
+	settings = {
+		pylsp = {
+			plugins = {
+				ruff = {
+					enabled = true,
+				},
+				black = {
+					enabled = true
+				},
+				autopep8 = {
+					enabled = false
+				},
+				yapf = {
+					enabled = false
+				},
+				pylint = {
+					enabled = false
+				},
+				pyflakes = {
+					enabled = false
+				},
+				pycodestyle = {
+					enabled = false
+				},
+				pylsp_mypy = {
+					enabled = false
+				},
+				jedi_completion = {
+					fuzzy = true
+				},
+				pyls_isort = {
+					enabled = true
+				},
+
+			}
+		}
+	}
+}
+
+local cmp = require('cmp')
+
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+	mapping = {
+		['<CR>'] = cmp.mapping.confirm({select = false}),
+		['<C-S-z>'] = cmp_action.tab_complete(),
+		['<C-S-x>'] = cmp_action.select_prev_or_fallback(),
+	},
+	preselect = 'item',
+	completion = {
+		completeopt = 'menu,menuone,noinsert',
+		
+	},
+
+})
--- lua/main_devel/lazy.lua
+++ lua/main_devel/lazy.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -28,5 +28,23 @@ require("lazy").setup({
         -- or                              , branch = '0.1.x',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-})
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {"ThePrimeagen/harpoon"},
+    {"mbbill/undotree"},
+    {
+        'VonHeikemen/lsp-zero.nvim', branch = 'v2.x',
+        dependencies = {
+            'neovim/nvim-lspconfig',
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+            'hrsh7th/nvim-cmp',
+            'hrsh7th/cmp-nvim-lsp',
+            'L3MON4D3/LuaSnip'
+        }
+    },
+    {'echasnovski/mini.surround'},
+    {'nvim-tree/nvim-web-devicons'},
+    {'lewis6991/gitsigns.nvim'},
+    {'romgrk/barbar.nvim'}
+}
+)
