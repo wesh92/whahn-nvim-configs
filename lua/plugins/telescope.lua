b300fa5d5e9fa36ff4ef294f6599b01fb4f3628b
Wes Hahn <52631646+wesh92@users.noreply.github.com>
2025-04-25 10:41:50 -0400

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,15 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fs', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.4',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
 
+    -- Define keymaps inside the config function
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<C-S-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>fs', function()
+        builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
+  end
+}
