e643642e74b2e0a8405679f2520bff80179250b1
Wes Hahn <52631646+wesh92@users.noreply.github.com>
2025-04-25 10:44:01 -0400

    Update undotree.lua

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,1 +1,8 @@
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+return {
+  'mbbill/undotree',
+  config = function()
+    vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+    -- Add any other undotree settings here
+    vim.g.undotree_WindowLayout = 2
+  end
+}
