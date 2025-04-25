55eb0ada9db027e78e589db7ac24bd74b8777b63
Wes Hahn <52631646+wesh92@users.noreply.github.com>
2025-04-25 10:37:29 -0400

    Create gitsigns.lua

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,11 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup {
+      -- Add any custom gitsigns configuration here
+      -- signs = { ... },
+      -- numhl = false,
+      -- ... other options
+    }
+  end
+}
